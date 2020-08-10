8778666791f75145958a3a33a04026e2b87a41db
estebancabreraa <esteban.cabrera.a@gmail.com>
2020-08-10 17:07:54 -0600

    Shuffle implementado en Deck

--- componets/deck.js
+++ componets/deck.js
@@ -32,4 +32,14 @@ class Deck {
         }
     }
 
+    shuffle(){
+        var j, i, temp;
+        for (i = this.stack.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            temp = this.stack[i];
+            this.stack[i] = this.stack[j];
+            this.stack[j] = temp;
+        }
+    }
+
 }
